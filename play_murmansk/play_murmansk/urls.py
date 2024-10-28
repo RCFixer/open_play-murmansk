06f480e531ec1904b21883779313267dfeb4205e
roman <roman.bogdanov@lanit-tercom.com>
2024-10-28 18:15:56 +0300

    Прототип главной страницы готов.

--- play_murmansk/play_murmansk/urls.py
+++ play_murmansk/play_murmansk/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from news.views import NewsListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', NewsListView.as_view(), name='index'),
 ]
