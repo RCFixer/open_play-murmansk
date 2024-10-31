fa2cebefd2974e5a113e2ebf968a8ee1152feaf1
roman <roman.bogdanov@lanit-tercom.com>
2024-10-31 19:41:26 +0300

    Прототип страницы обзоров игр.

--- play_murmansk/play_murmansk/urls.py
+++ play_murmansk/play_murmansk/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', NewsListView.as_view(), name='index'),
     path('board/', include('board.urls')),
+    path('blog/', include('reviews.urls')),
 ]
